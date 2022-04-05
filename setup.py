c687d7718fbc54dddba13900f645ae5bf8ebed59
Adam Serafini <adam.serafini@deliveryhero.com>
2022-04-05 10:42:37 +0200

    Module written in pure Zig that returns the integer "1"

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
+import os
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-zaml = Extension("zaml", sources=["zamlmodule.c"])
+zaml = Extension("zaml", sources=["zamlmodule.zig"])
+
+
+class ZigBuilder(build_ext):
+    def build_extension(self, ext):
+        assert len(ext.sources) == 1
+
+        if not os.path.exists(self.build_lib):
+            os.mkdir(self.build_lib)
+        self.spawn(
+            [
+                "zig",
+                "build-lib",
+                f"-femit-bin={self.get_ext_fullpath(ext.name)}",
+                "-fallow-shlib-undefined",
+                "-dynamic",
+                *[f"-I{d}" for d in self.include_dirs],
+                ext.sources[0],
+            ]
+        )
+
 
 setup(
     name="zaml",
     version="0.0.1",
     description="Fast YAML 1.2 Parser for Python 3.10.x",
     ext_modules=[zaml],
+    cmdclass={"build_ext": ZigBuilder},
 )
