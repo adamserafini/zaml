27b2d54ffb39aace5d5d58f0aa75396c3e6fe84d
Adam Serafini <adam.serafini@deliveryhero.com>
2022-04-05 14:21:16 +0200

    The simplest possible Zig extension for Python (#2)
    * Use setuptools - distutils is deprecated
    
    * The simplest C extension is actually...
    
    One that returns the integer "one"
    
    * We don't even need to accept arguments
    
    * Module written in pure Zig that returns the integer "1"
    
    * Only test for Mac OSX
    
    * Add Zig to CI
    
    * Build directory is nested
    
    * Try running on all 3x OS'es
    
    * Exclude linux from OS testing matrix
    
    * Only test on MacOSX

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
-from distutils.core import setup, Extension
+import os
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-module1 = Extension("zaml", sources=["zamlmodule.c"])
+zaml = Extension("zaml", sources=["zamlmodule.zig"])
+
+
+class ZigBuilder(build_ext):
+    def build_extension(self, ext):
+        assert len(ext.sources) == 1
+
+        if not os.path.exists(self.build_lib):
+            os.makedirs(self.build_lib)
+        self.spawn(
+            [
+                "zig",
+                "build-lib",
+                f"-femit-bin={self.get_ext_fullpath(ext.name)}",
+                "-fallow-shlib-undefined",
+                "-dynamic",
+                *[f"-I{d}" for d in self.include_dirs],
+                ext.sources[0],
+            ]
+        )
+
 
 setup(
     name="zaml",
     version="0.0.1",
     description="Fast YAML 1.2 Parser for Python 3.10.x",
-    ext_modules=[module1],
+    ext_modules=[zaml],
+    cmdclass={"build_ext": ZigBuilder},
 )
---
+++ zamlmodule.zig
@@ -0,0 +1,57 @@
+const py = @cImport({
+    @cDefine("PY_SSIZE_T_CLEAN", {});
+    @cInclude("Python.h");
+});
+
+const PyObject = py.PyObject;
+const PyMethodDef = py.PyMethodDef;
+const PyModuleDef = py.PyModuleDef;
+const PyModuleDef_Base = py.PyModuleDef_Base;
+const Py_BuildValue = py.Py_BuildValue;
+const PyModule_Create = py.PyModule_Create;
+const METH_NOARGS = py.METH_NOARGS;
+
+fn zaml_load(self: [*c]PyObject, args: [*c]PyObject) callconv(.C) [*]PyObject {
+    _ = self;
+    _ = args;
+    return Py_BuildValue("i", @as(c_int, 1));
+}
+
+var ZamlMethods = [_]PyMethodDef{
+    PyMethodDef{
+        .ml_name = "load",
+        .ml_meth = zaml_load,
+        .ml_flags = METH_NOARGS,
+        .ml_doc = "Load some tasty YAML.",
+    },
+    PyMethodDef{
+        .ml_name = null,
+        .ml_meth = null,
+        .ml_flags = 0,
+        .ml_doc = null,
+    },
+};
+
+var zamlmodule = PyModuleDef{
+    .m_base = PyModuleDef_Base{
+        .ob_base = PyObject{
+            .ob_refcnt = 1,
+            .ob_type = null,
+        },
+        .m_init = null,
+        .m_index = 0,
+        .m_copy = null,
+    },
+    .m_name = "zaml",
+    .m_doc = null,
+    .m_size = -1,
+    .m_methods = &ZamlMethods,
+    .m_slots = null,
+    .m_traverse = null,
+    .m_clear = null,
+    .m_free = null,
+};
+
+pub export fn PyInit_zaml() [*]PyObject {
+    return PyModule_Create(&zamlmodule);
+}
