7ca8155f382b75498ef1d9cc1633dda2c13ba634
Adam Serafini <adam@adamserafini.com>
2022-10-03 15:56:13 +0200

    C to virtual box

---
+++ zamlmodule.c
@@ -0,0 +1,28 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static PyObject *
+zaml_load(PyObject *self, PyObject *args)
+{
+    return Py_BuildValue("i", 1);
+}
+
+static PyMethodDef ZamlMethods[] = {
+    {"load", zaml_load, METH_VARARGS, "Load some tasty YAML."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+static struct PyModuleDef zamlmodule = {
+    PyModuleDef_HEAD_INIT,
+    "zaml",   /* name of module */
+    NULL,     /* module documentation, may be NULL */
+    -1,       /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+    ZamlMethods
+};
+
+PyMODINIT_FUNC
+PyInit_zaml(void)
+{
+    return PyModule_Create(&zamlmodule);
+}
