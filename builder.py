467a8f6e5a0c44e2dcaab4f3de0999e3d100ab80
Adam Serafini <adam@adamserafini.com>
2022-10-28 20:07:59 +0100

    No dll export fns

--- builder.py
+++ builder.py
@@ -79,6 +79,7 @@ class ZigCompiler:
                     "c",
                     "-dynamic",
                     "-fallow-shlib-undefined",
+                    "-fno-dll-export-fns",
                     f"-femit-bin={obj}",
                     *pp_opts,
                     src,
@@ -203,7 +204,7 @@ class ZigBuilder(build_ext):
         override_instance_method(self.compiler, "compile", ZigCompiler)
 
         original_link = self.compiler.link_shared_object
-        override_instance_method(self.compiler, "link_shared_object", ZigCompiler)
+        # override_instance_method(self.compiler, "link_shared_object", ZigCompiler)
         self.compiler.original_link = original_link
         self.compiler.src_extensions.append(".zig")
         super().build_extension(ext)
